8ac2e344021e09b468151c21d5d3fa2c5b5ca184
Shamil Rasheed <shamilrasheed131@gmail.com>
2023-01-22 22:12:05 +0530

    Frontend work started, fetched data from db

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar bg-light mb-4 p-2"></nav>
+      {/* <div className="container"> */}
+        <a href="/" className="navbar-brand">
+          ProjectMGMT
+        </a>
+      {/* </div> */}
+    </div>
+  )
+}
+
+export default Header
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const colors = require('colors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
